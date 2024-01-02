f84fe1d058a9d63cd505692bf2ea3bacd309ee6e
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2024-01-02 11:21:24 +0100

    working on innerpages

--- docs/statics/scripts/dynamiciframelink.js
+++ docs/statics/scripts/dynamiciframelink.js
@@ -1,4 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
     const dynamicLinks = document.querySelectorAll(".frame-link");
-    const frame = document.querySelector(".")
+    const frame = document.getElementById("dynframe");
+
+    dynamicLinks.forEach((link) => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault()
+
+            const newlink = link.getAttribute('data-src');
+            frame.src = newlink;
+
+            dynamicLinks.forEach((menus) => {
+                menus.classList.remove("active");
+                menus.classList.add("link-body-emphasis");
+            })
+
+            link.classList.add("active");
+            link.classList.remove("link-body-emphasis");
+
+        })
+    })
 })
